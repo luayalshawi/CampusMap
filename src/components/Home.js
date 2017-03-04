f4d7d56f3a78471514d3517967db9d038f638fd7
Luay Alshawi sp3 <loay.vb@gmail.com>
2017-03-04 13:07:43 -0800

    code refactor

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ import {
   AsyncStorage
 } from 'react-native';
 import {
-  Spinner,Picker,Grid,Row,H1 ,
+  Spinner,Picker,Grid,Row,H1 ,Col,
   Container, Header, Title, Content,
   Footer, FooterTab, Button, Left,
   Right, Body, Icon
@@ -21,8 +21,9 @@ import LocationsListView from './LocationsListView'
 import TabBar from './TabBar'
 import CampusMapView from './CampusMapView'
 import SearchBar from './SearchBar'
+import FirstTime from './FirstTime'
 import Ionicons from 'react-native-vector-icons/Ionicons';
-const Item = Picker.Item;
+
 
 export default class Home extends Component {
   constructor()
@@ -30,101 +31,78 @@ export default class Home extends Component {
     super()
     this.renderScene   = this.renderScene.bind(this)
     this.goTo          = this.goTo.bind(this)
-    this.onValueChange = this.onValueChange.bind(this)
     this.Routes = [
       {title: 'List View', index: 0},
     ];
     this.state = {
-      selected: 'osu',
-      startNavigation:null
+      startNavigation:null,
+      loading:true
     }
-  }
-  onPick(navigator)
-  {
-    //
-    // try {
-    //   AsyncStorage.setItem('@CampusMap:firstTime1', '1');
-    //   navigator.push({
-    //     index: 1,
-    //   })
-    //   this.goTo()
-    // } catch (error) {
-    //   // Error saving data
-    //   console.error(error);
-    // }
   }
   renderScene(route,navigator)
   {
     var index = route.index;
-
-    if(index==-1)
-    {
-      // This view is not used. Will be removed after completing the home page
-      return (
-        <View style={styles.firstcontainer}>
-          <View style={styles.flex1}>
-            <Text style={styles.welcomemsg}>Welcome To Campus Map</Text>
-          </View>
-          <View style={styles.picker}>
-            <Text style={styles.pickerText}>Please Choose a University</Text>
-            <Picker>
-              <Item label="Oregon State University - Corvallis" value="OSU" />
-              <Item label="world" value="key1" />
-            </Picker>
-          </View>
-          <View style={styles.flex3}>
-            <TouchableOpacity style={styles.button} onPress={this.onPick.bind(this,navigator)}>
-              <Text style={styles.pickerText}>Go</Text>
-            </TouchableOpacity>
-          </View>
-
-        </View>
-      )
-    }
-    else if(index == 0)
-    {
-      return (
-        <View style={{flex:1}}>
-          <Ionicons.ToolbarAndroid
-            style={styles.toolbar}
-            title="Home"
-            titleColor="white"
-            navIconName="md-menu"
-            onIconClicked={null}
-            actions={[]}
-            overflowIconName="md-menu"
-          />
-          <SearchBar/>
-          <LocationsListView navigator={navigator}/>
-        </View>
-      )
-    }
-    else if(index == 2)
-    {
-      return(
-        <View style={{flex:1}}>
-          <Ionicons.ToolbarAndroid
-            style={styles.toolbar}
-            title={route.title}
-            titleColor="white"
-            navIconName="md-arrow-back"
-            onIconClicked={navigator.pop}
-            actions={[]}
-            overflowIconName="md-arrow-back"
-          />
-          <CampusMapView
-            navigator={navigator}
-            route={route}
-          />
-        </View>
-      )
+    switch (index) {
+      case 0:
+        return (
+          <Container>
+              <Header>
+                  <Left>
+                      <Button transparent>
+                          <Icon name='menu' />
+                      </Button>
+                  </Left>
+                  <Body>
+                      <Title>Header</Title>
+                  </Body>
+                  <Right />
+              </Header>
+              <Content>
+                <SearchBar/>
+                <LocationsListView navigator={navigator}/>
+              </Content>
+          </Container>
+        )
+      case 2:
+        return(
+          // <View style={{flex:1}}>
+          //   <Ionicons.ToolbarAndroid
+          //     style={styles.toolbar}
+          //     title={route.title}
+          //     titleColor="white"
+          //     navIconName="md-arrow-back"
+          //     onIconClicked={navigator.pop}
+          //     actions={[]}
+          //     overflowIconName="md-arrow-back"
+          //   />
+          //   <CampusMapView
+          //     navigator={navigator}
+          //     route={route}
+          //   />
+          // </View>
+          <Container>
+              <Header>
+                  <Left>
+                      <Button transparent onPress={() => {navigator.pop}}>
+                          <Icon name='arrow-back' />
+                      </Button>
+                  </Left>
+                  <Body>
+                      <Title>Header</Title>
+                  </Body>
+                  <Right />
+              </Header>
+              <CampusMapView
+                navigator={navigator}
+                route={route}
+              />
+          </Container>
+        )
+      default:
+        return null
     }
   }
-  onValueChange (value: string) {
-    this.setState({
-        selected : value
-    });
-  }
+
   goTo()
   {
     try {
@@ -143,92 +121,62 @@ export default class Home extends Component {
   {
     try {
       const value =  AsyncStorage.getItem('@CampusMap:firstTime').then( (value) => {
-        if(value!=null)
-        {
-          this.setState({
-            startNavigation:true
-          })
-        }
-        else {
-          this.setState({
-            startNavigation:false
-          })
-        }
+        const val = (value!=null ? true:false);
+        this.setState({
+          startNavigation:val
+        })
       })
     } catch (error) {
       // Error retrieving data
       console.error(error);
     }
   }
+  componentDidMount()
+  {
+    setTimeout( () => {
+      this.setState({
+        loading:false
+      })
+    }, 1500);
+  }
   render() {
 
-    const {startNavigation} = this.state
-    if(startNavigation==true)
-    {
+    const {startNavigation,loading} = this.state
+    if (loading ==true || startNavigation==null){
+      return(
+        <Container>
+          <Grid>
+              <Row size={40}>
+                <Col></Col>
+                <Col><Text style={styles.loadingText}>CampusMap</Text></Col>
+                <Col></Col>
+              </Row>
+              <Row size={60}>
+                <Col></Col>
+                <Col><Spinner color='blue' /></Col>
+                <Col></Col>
+              </Row>
+          </Grid>
+        </Container>
+      )
+    }
+    switch (startNavigation) {
+      case true:
+        return (
+          <Navigator
+            initialRouteStack={this.Routes}
+            renderScene={this.renderScene}
+            configureScene={(route, routeStack) => Navigator.SceneConfigs.FadeAndroid}
+          />
+        );
+      case false:
       return (
-        <Navigator
-          initialRouteStack={this.Routes}
-          renderScene={this.renderScene}
-          configureScene={(route, routeStack) => Navigator.SceneConfigs.FadeAndroid}
-        />
-      );
-    }
-    else if(startNavigation==false){
-      return(
-        <Container>
-            <Header>
-                <Body>
-                    <Title>Campus Map</Title>
-                </Body>
-                <Right />
-            </Header>
-
-            <Content >
-              <Grid>
-                  <Row style={{ backgroundColor: '#635DB7', height: 200 }}>
-                    <Text style={styles.mainContent}>Welcome To Campus Map</Text>
-                  </Row>
-                  <Row style={{ backgroundColor: '#00CE9F',height: 50}}>
-                    <Text style={styles.mainContent}>Please pick a University</Text>
-                  </Row>
-                  <Picker
-                      iosHeader="Select one"
-                      mode="dropdown"
-                      selectedValue={this.state.selected}
-                      onValueChange={(value) => {this.onValueChange(value)}}>
-                      <Item label="Oregon State University" value="osu" />
-                      <Item label="ATM Card" value="key1" />
-                      <Item label="Debit Card" value="key2" />
-                      <Item label="Credit Card" value="key3" />
-                      <Item label="Net Banking" value="key4" />
-                 </Picker>
-
-              </Grid>
-
-            </Content>
-
-            <Footer>
-                <FooterTab>
-                    <Button full onPress={this.goTo}>
-                        <H1>Save</H1>
-                    </Button>
-
-                </FooterTab>
-            </Footer>
-        </Container>
+       <FirstTime onpress={this.goTo}/>
       )
-    }
-    else {
-      return(
-        <Container>
-            <Content>
-                <Spinner color='blue' />
-            </Content>
-        </Container>
-      )
-    }
+      default:
+        return null
 
-
+    }
   }
 }
 
@@ -278,13 +226,13 @@ const styles = StyleSheet.create({
     borderColor:'green',
     borderStyle:'solid'
   },
-  mainContent:{
-    textAlign:'center',
-    fontSize:25,
+  loadingText:{
+    textAlign: 'center',
+    alignItems: 'center',
+    fontSize:20,
     fontWeight:'bold',
-  },
-  pickerItem:{
-    width:300
+    color:'black'
   }
+
 
 });
